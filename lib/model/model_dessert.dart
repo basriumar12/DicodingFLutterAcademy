40b3147d216ee1408432eee6642a1d6d0704fea5
basri <basriumar12@gmail.com>
2019-05-04 10:24:59 +0700

    submission 2

--- lib/model/model_dessert.dart
+++ lib/model/model_dessert.dart
@@ -13,12 +13,36 @@ class ModelDessert {
 
 final List<ModelDessert> data = [
   ModelDessert('Nasi Goreng Merah',  'Nasi Goreng Merah dengan warna anti mainsetrum ! rasanya wow banget ', 'assets/img/nasigorengmerah.jpg'),
+
   ModelDessert('Nasi Goreng Seafood',  'Nasi Goreng Merah dengan warna anti mainsetrum ! rasanya wow banget ', 'assets/img/nasigorengseafood.jpg'),
+
   ModelDessert('Nasi Goreng Mawut',  'Nasi Goreng Mawut rasanya endesss ', 'assets/img/nasigorengmawut.jpg'),
+
   ModelDessert('Mie Ayam',  'Mie Ayam pakai ayam ! rasanya endesss ', 'assets/img/mieayam.jpg'),
+
   ModelDessert('Bakso',  'Bakso rasanya endesss ', 'assets/img/bakso.jpg'),
+
   ModelDessert('Ayam Geprek',  'Ayam Geprek dengan Geprekan Penuh Semangat biar rasanya nikmatttt !!! ', 'assets/img/ayamgeprek.jpg'),
+
   ModelDessert('Ayam Bakar',  'Ayam Bakar dengan Bakaran Penuh Perhatian biar jadi bahan perhatiann !!! ', 'assets/img/ayambakar.jpg'),
+
   ModelDessert('Ayam Kremez',  'Ayam Kremez dengan campuran Kremez niqmat menjadikan ayam tambah gemez ! ', 'assets/img/ayamkremes.jpg'),
-  ];
+
+  ModelDessert('Nasi Kuning',  'Nasi Kuning ada di semua daerah, biasanya di buat ketika ada yang berulang tahun', 'assets/img/nasikuning.jpg'),
+
+  ModelDessert('Nasi Padang',  'Siapa yang gak kenal nasi padang? makanan favorit kalo lagi lapar ya, wew nasi padang gak usah di tanya kelazatannya, di setiap daerah kita bisa jumpai warung padang dengan aneka ragam lauk dan sayurnya yang enak', 'assets/img/nasipadang.jpg'),
+
+  ModelDessert('Nila Bakar',  'Ikan nila, siapa yang sudah pernah rasa ikan satu ini, ikan nila enak di bakar dan di sajikan dengan sambal roa yang pedas banget', 'assets/img/nila.jpg'),
+
+  ModelDessert('Onde Bulat',  'Onde-onde, satu jajanan ini enak loh, familiar kita dapatin di street food, isinya macam-macam, ada kacang hijau, coklat, keju dan lain sebagainya', 'assets/img/onde.jpg'),
+
+  ModelDessert('Popolulu Gorontalo',  'Popolulu, ada yag tau jajanan ini ? ini jajanan berasal dari gorontalo loh, jajanan ini enak banget, apalagi dimakan ketika masih panas, gak kebayang lezatnya. Biasanya isinya ada ubi dan kacang ijo', 'assets/img/popolulu.jpg'),
+
+  ModelDessert('Soto Solo',  'Soto, makanan favorit kalo mau saranpan dan makan malam. Soto,salah satu street food yang sering kita temui di jalanan yah, rasanya khas banget dan enak jika dimakan dikala cuaca dingin', 'assets/img/soto.jpg'),
+
+  ModelDessert('Tuna Bakar',  'Ikan Tuna, enakng banget rasanya dan sedap,ada yang pernah makan ikan tuna? ikan ini salah satu spesies ikan besar yang dagingnya enak banget ya, karena ikan ini bisa di buat apa aja, dibakar, di goreng apalagi di sate pakai dabu-dabu pedas, woww....!! ', 'assets/img/tuna.jpg'),
+
+
+
+];
 
